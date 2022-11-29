3b1538f63735086d8c6740d14316bd6f94397c1a
Gabriella-Luu <luyihan19@mails.ucas.ac.cn>
2022-11-29 00:49:13 -0800

    rearrange

--- scripts/mint.js
+++ scripts/mint.js
@@ -5,13 +5,11 @@ const nftBuild = require('../build/contracts/NFT.json')
 const nftContract = new web3.eth.Contract(nftBuild.abi, nftBuild.networks[5777].address)
 console.log(`address: ${nftBuild.networks[5777].address}\n`)
 
-const gas = 450000
-
 const main = async () => {
     const [account] = await web3.eth.getAccounts()
     const baseCost = await nftContract.methods.cost().call()
     console.log(`Attempting to mint NFT...\n`)
-    await nftContract.methods.mint().send({ from: account, value: baseCost, gas: gas })
+    await nftContract.methods.mint().send({ from: account, value: baseCost, gas: 450000 })
     const totalMinted = await nftContract.methods.walletOfOwner(account).call()
     const uri = await nftContract.methods.tokenURI(totalMinted[0]).call()
     console.log(`Metadata URI for token: ${uri}\n`)
